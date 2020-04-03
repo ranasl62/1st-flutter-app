39ad92ad23c6321f742bc347e25c94af6ef2ebc1
ranasl62 <ranasl62@gmail.com>
2020-04-03 19:35:00 +0600

    render conditionally more properway

--- lib/product.dart
+++ lib/product.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Product extends StatelessWidget {
   final List<String> products;
   Product([this.products = const []]); //optional arguments mention []
+
+
   Widget _buildProductItem(BuildContext contex, int index) {
     return Card(
       child: Column(
@@ -13,12 +15,20 @@ class Product extends StatelessWidget {
       ),
     );
   }
-
+  Widget _buildProductList() {
+    Widget productCard = Center(
+      child: Text('No Food Found, please add some'),
+    );
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    }
+    return productCard;
+  }
   @override
   Widget build(BuildContext context) {
-    return products.length > 0 ? ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    ):Center(child: Text('No Food Found, please add some'),);
+    return _buildProductList();
   }
 }
