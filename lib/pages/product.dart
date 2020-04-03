2504c120436347d3ee0d2eb0ef24fa45f238c022
ranasl62 <ranasl62@gmail.com>
2020-04-03 23:02:51 +0600

    Rection on button pressing

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,28 +8,34 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // print(widget.);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(title),
-          ),
-          Container(
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
               padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('Delete'),
-                onPressed: () {
-                  Navigator.pop(context, true);
-                },
-              ))
-        ],
+              child: Text(title),
+            ),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Delete'),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                ))
+          ],
+        ),
       ),
     );
   }
