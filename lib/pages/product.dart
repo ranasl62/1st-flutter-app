cc5fd6feaa4e64e0793a1cfa0571cd633543732e
ranasl62 <ranasl62@gmail.com>
2020-04-05 01:22:11 +0600

    alert and modal show

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,6 +4,32 @@ class ProductPage extends StatelessWidget {
   final String title;
   final String imageUrl;
   ProductPage({this.title, this.imageUrl});
+  _showDialog(context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action cannot be undone!'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('DISCARD'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('CONTINUE'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     // print(widget.);
@@ -35,9 +61,7 @@ class ProductPage extends StatelessWidget {
                     child: RaisedButton(
                       color: Theme.of(context).accentColor,
                       child: Text('Delete'),
-                      onPressed: () {
-                        Navigator.pop(context, true);
-                      },
+                      onPressed: () => _showDialog(context),
                     ))
               ],
             ),
